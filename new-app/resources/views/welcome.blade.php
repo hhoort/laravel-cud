cb5173a0be833c6440933cbe9ebce7a09adec1aa
hhoort <hhoor6943@gmail.com>
2023-12-05 18:50:06 +0500

    create a file

--- new-app/resources/views/welcome.blade.php
+++ new-app/resources/views/welcome.blade.php
@@ -702,3 +702,82 @@
 <!-- end info section -->
 
 @endsection
+
+
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/solid.min.css">
+</head>
+<body>
+    
+<div class="container">
+
+    <h1>Register Form</h1>
+    <form action="{{url('/')}}/form" method="POST" class="form-group">
+        @csrf
+        <label for="name">Name</label>
+        <input type="text" name="name" class="form-control" value="{{old('name')}}">
+        <span class="text-danger">
+            @error('name')
+                {{$message}}
+            @enderror
+        </span>
+        <br>
+        <label for="email">Email</label>
+        <input type="email" name="email" class="form-control" value="{{old('email')}}">
+        <span class="text-danger">
+            @error('email')
+            {{$message}}
+            @enderror
+        </span>
+        <br>
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control" value="{{old('password')}}">
+        <span class="text-danger">
+            @error('password')
+            {{$message}}
+            @enderror
+        </span>
+        <br><br>
+        <input type="submit" name="submit" class="btn btn-success"  value="register">
+
+    </form>
+</div> --}}
+
+
+{{-- <div class="container"mt-5>
+    <center>
+    <h1>students records</h1>
+</center>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Password</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach ($stddata as $value)
+          <tr>
+            <th scope="row">{{$value->id}}</th>
+            <th scope="row">{{$value->name}}</th>
+            <td>{{$value->email}}</td>
+            <td>{{$value->password}}</td>
+            <td><a href="{{url('/')}}/std/dlt/{{$value->id}}"><i class="fa-solid fa-xmark"></i></a></td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+</div>  --}}
+{{-- </body>
+</html> --}}
--- new-app/routes/web.php
+++ new-app/routes/web.php
@@ -1,23 +1,58 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\welcomecontroller;
-use App\Http\Controllers\aboutcontroller;
-use App\Http\Controllers\teamcontroller;
-use App\Http\Controllers\servicecontroller;
-
+// use app\Http\Controllers \ customcontroller;
+// use App\Http\Controllers\customcontroller;
+// use App\Http\Controllers\formcontroller;
+// use App\Http\Controllers\singleaction;
+use App\Http\Controllers\formcontroller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/home', [welcomecontroller::class, 'index']);
-Route::get('/about', [aboutcontroller::class, 'about']);
-Route::get('/team', [teamcontroller::class, 'team']);
-Route::get('/service', [servicecontroller::class, 'service']);
+// parameter pass in Route::get("/", function () {
+// Route::get('/home/{name?}/{age?}', function ($name = null , $age = null) {
+//     return view('welcome')->with(compact('name' , 'age'));
+    // $data = compact('name' , 'age')
+    // print_r($data);
+// });
+
+// url route DirectoryRoute::get('')
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+// Route::get('/logout', function () {
+//     return view('logout');
+// });
+
+// Route::get('/home',[customcontroller::class,'index']);
+// Route::get('/about',[customcontroller::class,'about']);
+
+// Route::controller(customcontroller::class)->group(function(){
+// Route::get('/','index')->name('home');
+// Route::get('/about','about')->name('about');
+// Route::get('/contact','contact')->name('contact');
+// });
+// Route::get('/welcome',singleaction::class)->name('home');
+//  Route::get('/about',singleaction::class)->name('about');
+//  Route::get('/contact',singleaction::class)->name('contact');
+
+Route::get('/form',[formcontroller::class,'register']);
+Route::post('/form',[formcontroller::class,'data']);
+Route::get('/std/view',[formcontroller::class,'std_view']);
+
+
+Route::get('/std/dlt/{id}',[formcontroller::class,'std_dlt']);
+
